b85d7c8277fb02955027ebd896ff3cfc1699f97b
Dani Ruiz <dani.ruiz.89@gmail.com>
2013-09-21 11:31:13 +0000

    Trying to make it work, but nothing

--- 1_Hurd/procadv.c
+++ 1_Hurd/procadv.c
@@ -2,6 +2,8 @@
 #include <mach_error.h>
 #include <mach/mig_errors.h>
 #include <mach/thread_status.h>
+#include <mach/processor_info.h>
+#include <mach/mach_host.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <hurd.h>
@@ -15,6 +17,9 @@ int main ()
    int res, i;
    mach_port_t host_privileged_port; 
    device_t device_privileged_port;
+   mach_port_t* host;
+   mach_msg_type_number_t procInfoCnt;
+   processor_info_t procInfo;
 
    res = get_privileged_ports(&host_privileged_port, &device_privileged_port);
    if (res != KERN_SUCCESS) {
@@ -39,12 +44,18 @@ int main ()
    printf ("        processors at array 0x%x\n", processor_list);
    printf ("processor_listCnt %d\n", processor_listCnt);
 
-   for (i=0; i < processor_listCnt; i++)
-      printf ("processor_list[%d] 0x%x\n", i, processor_list[i]);
+	procInfo = (processor_info_t) malloc(512*sizeof(int));
+	host = (mach_port_t *) malloc(10*sizeof(mach_port_t));
+
+   for (i=0; i < processor_listCnt; i++) {
+	   printf ("processor_list[%d] 0x%x\n", i, processor_list[i]);
+
+	   procInfoCnt = processor_listCnt;
+	   res = processor_info (processor_list[i], PROCESSOR_BASIC_INFO, host, procInfo, &procInfoCnt);
+	   if (res != KERN_SUCCESS) {
+		   fprintf (stderr, "Error getting the processor %d information (0x%x), %s\n", i, res, mach_error_string(res));
+		   exit(-1);
+	   }
+   }
 }
-
- 
-
-
-   
 
