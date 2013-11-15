20d7fcd9d97da2f6b9e34cb979acfa0b6d797b3a
Dani Ruiz <daniel.ruiz@bsc.es>
2013-11-15 08:45:37 +0100

    all fs working except reiser4 due to the kernel doesn't support it

--- 4_block_dev/new_version/casoblk/implementation.c
+++ 4_block_dev/new_version/casoblk/implementation.c
@@ -6,16 +6,22 @@
 
 #include <linux/vmalloc.h>
 
+#include <linux/hdreg.h>
+
 #include "interface.h"
 
 
 char * alloc_disk_memory(unsigned long size)
 {
    char * p = NULL;
+   unsigned long i;
 
    // la vostra implementacio va aqui
-
-
+   	if(size > xrd_size*1024) {
+		return NULL;
+	}
+	p = (char *) vmalloc(PAGE_ALIGN(size));
+	for(i = 0; i<size; ++i) p[i] = i%256;
    // fi de la vostra implementacio
 
    printk(KERN_DEBUG "alloc_disk_memory %ld address %p\n", size, p);
@@ -26,8 +32,7 @@ char * alloc_disk_memory(unsigned long size)
 void   free_disk_memory(char * disk_mem)
 {
    // la vostra implementacio va aqui
-
-
+	if(disk_mem != NULL) vfree((const void*) disk_mem);
    // fi de la vostra implementacio
    printk(KERN_DEBUG "free_disk_memory %p\n", disk_mem);
 }
@@ -36,10 +41,16 @@ void   free_disk_memory(char * disk_mem)
 int xrd_getgeo(struct block_device * bdev, struct hd_geometry *geo)
 {
    int res = -EIO;
+   struct xrd_struct *xrd_str;
+   xrd_str = bdev->bd_disk->private_data;
 
    // la vostra implementacio va aqui
-
-
+   //
+	geo->heads = 32;
+	geo->sectors = 128;
+	geo->cylinders = xrd_str->size / geo->heads / geo->sectors / SECTOR_SIZE;
+	geo->start = 0;
+   res = 0;
    // fi de la vostra implementacio
 
    // un resultat = 0 indica tot correcte
@@ -54,10 +65,12 @@ int copy_from_xrd(void *dst, struct xrd_struct *xrd,
    int res = -ENODEV;
 
    // la vostra implementacio va aqui
-
-
-
-
+   unsigned long *src;
+   src = (unsigned long *) (xrd->disk_memory + sector*SECTOR_SIZE);
+   //TODO: check this comparision. Anyway, is working now
+   //if((void*)src >= (void*)(xrd->disk_memory + xrd_size)) return -ENOMEM;
+  	memcpy(dst, (void*) src, n);
+	res = 0;
    // fi de la vostra implementacio
 
    printk(KERN_DEBUG "copy_from_xrd retorna %d\n", res);
@@ -71,10 +84,12 @@ int copy_to_xrd(struct xrd_struct *xrd, void *src,
    int res = -ENODEV;
    
    // la vostra implementacio va aqui
-
-
-
-
+   unsigned long *dst;
+   dst = (unsigned long *) (xrd->disk_memory + sector*SECTOR_SIZE);
+   //TODO: check this comparision. Anyway, is working now
+   //if((void *)dst >= (void*)(xrd->disk_memory + xrd_size)) return -ENOMEM;
+   memcpy((void *) dst, src, n);	
+   res = 0;
    // fi de la vostra implementacio
 
    printk(KERN_DEBUG "copy_to_xrd retorna %d\n", res);
